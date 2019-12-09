b433f8c585a3959fe6ce60cb3cc38d1c26c3bfd4
Avery Harnish <averyharnish@gmail.com>
2019-12-08 18:54:47 -0600

    Day two part two

--- src/days/two/mod.rs
+++ src/days/two/mod.rs
@@ -2,29 +2,44 @@ mod input;
 use input::INPUT;
 
 pub fn solution() -> Result<(), failure::Error> {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let result = run(noun, verb)?;
+            if result == 19690720 {
+                println!("{}", 100 * noun + verb);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn run(noun: usize, verb: usize) -> Result<usize, failure::Error> {
     let mut program: Vec<usize> = INPUT
         .split(",")
         .map(|x| x.trim().parse::<usize>().unwrap())
         .collect();
-    program[1] = 12;
-    program[2] = 2;
-    let mut position = 0;
-    while program[position] != 99 {
-        let opcode = program[position];
-        let position_one = program[position + 1];
-        let position_two = program[position + 2];
-        let result_position = program[position + 3];
-        match opcode {
-            1 => {
-                program[result_position] = program[position_one] + program[position_two];
-            }
-            2 => {
-                program[result_position] = program[position_one] * program[position_two];
-            }
-            _ => failure::bail!("unexpected opcode {}", opcode),
-        }
-        position += 4;
+    program[1] = noun;
+    program[2] = verb;
+    let mut instruction_pointer = 0;
+    while program[instruction_pointer] != 99 {
+        let instruction = &program.clone()[instruction_pointer..instruction_pointer + 4];
+        process(&mut program, instruction)?;
+        instruction_pointer += 4;
     }
-    println!("{}", program[0]);
+    Ok(program[0])
+}
+
+fn process(program: &mut Vec<usize>, instruction: &[usize]) -> Result<(), failure::Error> {
+    let opcode = instruction[0];
+    let parameters = &instruction[1..];
+    match opcode {
+        1 => {
+            program[parameters[2]] = program[parameters[0]] + program[parameters[1]];
+        }
+        2 => {
+            program[parameters[2]] = program[parameters[0]] * program[parameters[1]];
+        }
+        _ => failure::bail!("unexpected opcode {}", opcode),
+    };
     Ok(())
 }
